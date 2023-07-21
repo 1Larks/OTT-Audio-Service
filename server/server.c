222972cf4bbf8cffffe88172305ec6049fa70bcc
Ori Benado <73161812+1Larks@users.noreply.github.com>
2023-07-21 03:28:00 +0300

    server.c

--- server/server.c
+++ server/server.c
@@ -167,8 +167,6 @@ int main(){
     //initialise server socket
     sockfd=initSockfd(address);
 
-    //set master socket to allow multiple connections , 
-    //this is just a good habit, it will work without this 
     if( setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 ){  
         error("error in setsockopt");  
     }  
